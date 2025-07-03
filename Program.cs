0e7d04934122c70abbaed44ea24f24f084c47e0b
ahmed123255 <ahmedsabermekawy@gmail.com>
2025-07-04 00:29:37 +0300

    Commit summary: Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace YearToDays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter number of years:");
+            int years = 0;
+            bool isConverted = int.TryParse(Console.ReadLine(), out years);
+
+            if (isConverted && years >= 0)
+            {
+                int days = years * 365;
+                int weeks = years * 52;
+                int months = years * 12;
+                int hours = days * 24;
+                int prayers = days * 5;
+                int rak3at = days * 17;
+                int jum3at = weeks;
+
+                Console.WriteLine("Number of months: " + months);
+                Console.WriteLine("Number of weeks: " + weeks);
+                Console.WriteLine("Number of days: " + days);
+                Console.WriteLine("Number of hours: " + hours);
+                Console.WriteLine("Number of prayers: " + prayers);
+                Console.WriteLine("Number of rak3at: " + rak3at);
+                Console.WriteLine("Number of jum3at (Fridays): " + jum3at);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number of years.");
+            }
+        }
+    }
+}
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# YearToDays
-A C# console app that converts age in years into months, weeks, days, and prayer statistics. 
